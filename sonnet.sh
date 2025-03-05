212ae8ce25d684ce845307135616c454d146c13a
Auro Tripathy <ipserv@yahoo.com>
2025-03-05 15:55:54 -0800

    init

---
+++ sonnet.sh
@@ -0,0 +1,12 @@
+python benchmarks/benchmark_serving.py \
+       --backend openai-chat \
+       --endpoint /v1/chat/completions \
+       --tokenizer meta-llama/Meta-Llama-3-8B-Instruct \
+       --model meta-llama/Meta-Llama-3-8B-Instruct \
+       --result-dir results \
+       --dataset-name sonnet \
+       --dataset-path benchmarks/sonnet.txt \
+       --sonnet-input-len 200 \
+       --sonnet-output-len 500 \
+       --sonnet-prefix-len 24 \
+       --num-prompts 1
