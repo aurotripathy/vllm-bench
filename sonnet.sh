3936edfcce518add0675a0b1e25715449574bee6
Auro Tripathy <ipserv@yahoo.com>
2025-03-10 17:30:16 -0700

    initial working with sonnet dataset

--- sonnet.sh
+++ sonnet.sh
@@ -1,12 +1,25 @@
+# python benchmarks/benchmark_serving.py \
+#        --backend openai \
+#        --endpoint /v1/completions \
+#        --tokenizer meta-llama/Meta-Llama-3-8B-Instruct \
+#        --model meta-llama/Meta-Llama-3-8B-Instruct \
+#        --result-dir results \
+#        --dataset-name sonnet \
+#        --dataset-path benchmarks/sonnet.txt \
+#        --sonnet-input-len 200 \
+#        --sonnet-output-len 500 \
+#        --sonnet-prefix-len 24 \
+#        --num-prompts 1
+
 python benchmarks/benchmark_serving.py \
-       --backend openai-chat \
-       --endpoint /v1/chat/completions \
+       --backend openai \
+       --endpoint /v1/completions \
        --tokenizer meta-llama/Meta-Llama-3-8B-Instruct \
-       --model meta-llama/Meta-Llama-3-8B-Instruct \
+       --model EMPTY \
        --result-dir results \
        --dataset-name sonnet \
        --dataset-path benchmarks/sonnet.txt \
        --sonnet-input-len 200 \
        --sonnet-output-len 500 \
        --sonnet-prefix-len 24 \
-       --num-prompts 1
+       --num-prompts 100
