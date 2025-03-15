96f718a2a29c6938b6a8cde0c6d18b97aa8a33d6
Auro Tripathy <ipserv@yahoo.com>
2025-03-14 19:04:10 -0700

    final config

--- sonnet.sh
+++ sonnet.sh
@@ -1,25 +1,20 @@
-# python benchmarks/benchmark_serving.py \
-#        --backend openai \
-#        --endpoint /v1/completions \
-#        --tokenizer meta-llama/Meta-Llama-3-8B-Instruct \
-#        --model meta-llama/Meta-Llama-3-8B-Instruct \
-#        --result-dir results \
-#        --dataset-name sonnet \
-#        --dataset-path benchmarks/sonnet.txt \
-#        --sonnet-input-len 200 \
-#        --sonnet-output-len 500 \
-#        --sonnet-prefix-len 24 \
-#        --num-prompts 1
+
+# note the server script 
+# furiosa-llm serve ./Llama-3.1-8B-Instruct-FP8 --devices "npu:0" --enable-auto-tool-choice --tool-call-parser llama3_json --chat-template chat-template.txt -tp 8
 
 python benchmarks/benchmark_serving.py \
+       --host localhost \
+       --port 8000 \
        --backend openai \
        --endpoint /v1/completions \
+       --request-rate 10 \
        --tokenizer meta-llama/Meta-Llama-3-8B-Instruct \
-       --model EMPTY \
-       --result-dir results \
+       --model Meta-Llama-3-8B-Instruct \
+       --save-result \
+       --result-dir ./results \
        --dataset-name sonnet \
        --dataset-path benchmarks/sonnet.txt \
-       --sonnet-input-len 200 \
-       --sonnet-output-len 500 \
-       --sonnet-prefix-len 24 \
-       --num-prompts 100
+       --sonnet-input-len 1024 \
+       --sonnet-output-len 70 \
+       --max-concurrency 1 \
+       --num-prompts 50
